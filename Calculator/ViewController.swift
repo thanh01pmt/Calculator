682ba3dcc42eaf16ccb9448a79d3d32ff8ccba41
Tony Pham <thanh01.pmt@gmail.com>
2024-07-13 17:01:17 +0700

    add functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,13 +7,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var lblResult: UILabel!
+    // A tip to check data type
+    // lblResult.text = String(describing: type(of: sender.titleLabel!.text!))
     
-    @IBAction func clickBtn1(_ sender: UIButton) {
-//        lblResult.text = String(describing: type(of: sender.titleLabel!.text!))
+    
+    @IBAction func clickBtn0(_ sender: UIButton) {
     }
     
+    @IBAction func clickBtn1(_ sender: UIButton) {
+    }
     
+    @IBAction func clickBtn2(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtn3(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtn4(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtn5(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtn6(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtn7(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtn8(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtn9(_ sender: UIButton) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
