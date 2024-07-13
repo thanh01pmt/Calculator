2be011ab2e41a3e44b7c0d205704edc0439a6e19
Tony Pham <thanh01.pmt@gmail.com>
2024-07-13 17:12:01 +0700

    add all functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,9 +7,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //--------------------------------//
+    // Constants and Varibles
+    let result: Float = 0.0
+    let preResult: Float = 0.0
+    let formula: String = ""
+    
+    //--------------------------------//
+    // Outlets
     // A tip to check data type
     // lblResult.text = String(describing: type(of: sender.titleLabel!.text!))
+
+    @IBOutlet weak var lblFormula: UILabel!
+    @IBOutlet weak var lblResult: UILabel!
     
+    //--------------------------------//
+    // Support Function
+    
+    //--------------------------------//
+    // Number Button Action
+    // A tip to check data type
+    // lblResult.text = String(describing: type(of: sender.titleLabel!.text!))
     
     @IBAction func clickBtn0(_ sender: UIButton) {
     }
@@ -41,9 +59,57 @@ class ViewController: UIViewController {
     @IBAction func clickBtn9(_ sender: UIButton) {
     }
     
+    @IBAction func clickBtnDot(_ sender: UIButton) {
+    }
+    
+    //--------------------------------//
+    // Function Button Action
+    
+    @IBAction func clickBtnAllClear(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtnDeleteLast(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtnPlus(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtnMinus(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtnMultiply(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtnDivide(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickBtnEqual(_ sender: UIButton) {
+    }
+    
+    
+    //--------------------------------//
+    // Support Function
+    
+    func addToFormula(_ str: String) {
+        
+    }
+
+    func removeLastCharacter() {
+        
+    }
+    
+    func calculate() {
+        
+    }
+    
+    func allClear() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 }
 
